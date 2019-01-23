a18f2c25a5488dabbb51ae45c08145087534edc8
Grégory Copin <gregcop1@gmail.com>
2019-01-23 14:52:56 +0100

    Updates

---
+++ precache-manifest.d5db52268cca80774a4f05c90a03d2c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd4d9b048174893eb301",
+    "url": "/twitter-react/static/js/runtime~main.dd4d9b04.js"
+  },
+  {
+    "revision": "714bde40d908c444ecc8",
+    "url": "/twitter-react/static/js/main.714bde40.chunk.js"
+  },
+  {
+    "revision": "87139015c954250d4b32",
+    "url": "/twitter-react/static/js/1.87139015.chunk.js"
+  },
+  {
+    "revision": "714bde40d908c444ecc8",
+    "url": "/twitter-react/static/css/main.d077f835.chunk.css"
+  },
+  {
+    "revision": "cf96376b8dea7df95d1cc803175ab1e9",
+    "url": "/twitter-react/index.html"
+  }
+];
